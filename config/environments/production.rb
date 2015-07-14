efd3770ae923537a725d7620a5c091cddab098b1
Jeff Bryan <JBryan5733@gmail.com>
2015-07-13 19:38:44 -0500

    added bootstrap sprockets to fix some graphical issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # change this to actual host name asap!!!
-  config.action_mailer.default_url_options = { :host =>'localhost:3000' }
+  config.action_mailer.default_url_options = { :host =>'https://quiet-bastion-7268.herokuapp.com/' }
 end
