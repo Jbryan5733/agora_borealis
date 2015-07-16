d6cde0d2503556adfd8df3725e1e177c2f16b688
Jeff Bryan <JBryan5733@gmail.com>
2015-07-15 20:08:14 -0500

    added blog basic functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :events
+  resources :events, :articles
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'pages#home'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714012251) do
+ActiveRecord::Schema.define(version: 20150715232615) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
