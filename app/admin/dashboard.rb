caa5a3f11db7f22b61af0fa16a5718ee7d965ae0
Jeff Bryan <JBryan5733@gmail.com>
2015-07-28 00:09:32 -0500

    removed underlines from links added image to about us page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,33 +2,24 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content do
+      section "Your tasks for this week" do
+        table_for current_admin_user.tasks.where('due_date > ? and due_date < ?', Time.now, 1.week.from_now) do |t|
+          t.column("Status") { |task| status_tag (task.is_done ? "Done" : "Pending"), (task.is_done ? :ok : :error) }
+          t.column("Title") { |task| link_to task.title, admin_task_path(task) }
+          t.column("Assigned To") { |task| task.admin_user.email }
+          t.column("Due Date") { |task| task.due_date? ? l(task.due_date, :format => :long) : '-' }
+        end
+      
+     
+      section "Tasks that are late" do
+        table_for current_admin_user.tasks.where('due_date < ?', Time.now) do |t|
+          t.column("Status") { |task| status_tag (task.is_done ? "Done" : "Pending"), (task.is_done ? :ok : :error) }
+          t.column("Title") { |task| link_to task.title, admin_task_path(task) }
+          t.column("Assigned To") { |task| task.admin_user.email }
+          t.column("Due Date") { |task| task.due_date? ? l(task.due_date, :format => :long) : '-' }
+        end
       end
-    
-    end
-
-
-  section "Your tasks for this week" do
-    table_for current_admin_user.tasks.where('due_date > ? and due_date < ?', Time.now, 1.week.from_now) do |t|
-      t.column("Status") { |task| status_tag (task.is_done ? "Done" : "Pending"), (task.is_done ? :ok : :error) }
-      t.column("Title") { |task| link_to task.title, admin_task_path(task) }
-      t.column("Assigned To") { |task| task.admin_user.email }
-      t.column("Due Date") { |task| task.due_date? ? l(task.due_date, :format => :long) : '-' }
-    end
-  
- 
-  section "Tasks that are late" do
-    table_for current_admin_user.tasks.where('due_date < ?', Time.now) do |t|
-      t.column("Status") { |task| status_tag (task.is_done ? "Done" : "Pending"), (task.is_done ? :ok : :error) }
-      t.column("Title") { |task| link_to task.title, admin_task_path(task) }
-      t.column("Assigned To") { |task| task.admin_user.email }
-      t.column("Due Date") { |task| task.due_date? ? l(task.due_date, :format => :long) : '-' }
-    end
-  end
 end
     
   end # content
